4e133d06ca31c31c0583e010e318b9ac270e40a1
rgan10566 <rganesan@beachbody.com>
2020-02-26 17:55:02 -0800

    end of day 2/26

--- exercise_pymsql.py
+++ exercise_pymsql.py
@@ -6,94 +6,107 @@ import pymysql.cursors
 import mconfig
 
 
+def initconnect(pconfig, log):
 
-def initialize(sconfig):
-    sconfig.db_host='tablette-1.culomlubyiwb.us-west-2.rds.amazonaws.com'
-    sconfig.db_user='app_admin'
-    sconfig.db_password='BB_Tabl3tt3'
-    sconfig.db_port='3306'
-    sconfig.db_name='tablette'
-    return True
+    try:
+# Connect to the database
+        print("connecting to ",pconfig[0],pconfig[3], pconfig[1],pconfig[4])
+        connection = pymysql.connect(host=pconfig[0],
+                                port=pconfig[3],
+                                user=pconfig[1],
+                                password=pconfig[2],
+                                database=pconfig[4])
 
+    except:
+        log.error("ERROR: Unexpected error: Could not connect to Aurora instance.")
+        return -1
 
-    # def open_connection(self):
-    #     """Connect to MySQL Database."""
-    #     try:
-    #         if self.conn is None:
-    #             self.conn = pymysql.connect(self.host,
-    #                                         user=self.username,
-    #                                         passwd=self.password,
-    #                                         db=self.dbname,
-    #                                         connect_timeout=5)
-    #     except pymysql.MySQLError as e:
-    #         logging.error(e)
-    #         sys.exit()
-    #     finally:
-    #         logging.info('Connection opened successfully.')
-    #
-    # def run_query(self, query):
-    #     """Execute SQL query."""
-    #     try:
-    #         self.open_connection()
-    #         with self.conn.cursor() as cur:
-    #             if 'SELECT' in query:
-    #                 records = []
-    #                 cur.execute(query)
-    #                 result = cur.fetchall()
-    #                 for row in result:
-    #                     records.append(row)
-    #                 cur.close()
-    #                 return records
-    #             else:
-    #                 result = cur.execute(query)
-    #                 self.conn.commit()
-    #                 affected = f"{cur.rowcount} rows affected."
-    #                 cur.close()
-    #                 return affected
-    #     except pymysql.MySQLError as e:
-    #         print(e)
-    #     finally:
-    #         if self.conn:
-    #             self.conn.close()
-    #             self.conn = None
-    #             logging.info('Database connection closed.')
+    return connection
+
+def runquery(conn, query, log):
+
+    try:
+        with conn.cursor() as cur:
+                records = []
+                cur.execute(query)
+                result = cur.fetchall()
+                for row in result:
+                    records.append(row)
+    except pymysql.MySQLError as e:
+        log.error(e)
+        return []
+
+    return records
+
+def inserttable(conn, table, data, log):
+        sql="insert into "+table+" ("
+        try:
+            with conn.cursor() as cur:
+                    for i in 1..len(data):
+                        for k in range(len(data[0])):
+                            sql = sql + data[0][i]
+                        sql = sql + ") values"
+                        for l in range(len(data[i])):
+                            sql = sql + data[i][l]
+                        sql = sql + ")"
+                    print(sql)
+                    query=sql
+                    cur.execute(query)
+        except pymysql.MySQLError as e:
+            log.error(e)
+            return False
+
+        return True
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-try:
-# Connect to the database
-        connection = pymysql.connect(host='tablette.cluster-culomlubyiwb.us-west-2.rds.amazonaws.com',
-                                port=3306,
-                                user='appadmin',
-                                password='BB_Tabl3tt3',
-                                database='tablette')
+conn=initconnect(mconfig.tablette,logger)
 
-except:
-        logger.error("ERROR: Unexpected error: Could not connect to Aurora instance.")
-        sys.exit()
+if conn != -1:
+    if inserttable(conn,"users",[['email','password'],['rame10566@gmail.com','Secret'],['rgan10566@yahoo.com','pesterme']],logger):
+        res=runquery(conn, "select * from users",logger)
+        print(res)
+else:
+    print("Connection not establised")
 
-logger.info("Success: connection to aurora established")
+
+
+
 
 # try:
+# # Connect to the database
+#         connection = pymysql.connect(host='tablette.cluster-culomlubyiwb.us-west-2.rds.amazonaws.com',
+#                                 port=3306,
+#                                 user='appadmin',
+#                                 password='BB_Tabl3tt3',
+#                                 database='tablette')
+#
+# except:
+#         logger.error("ERROR: Unexpected error: Could not connect to Aurora instance.")
+#         sys.exit()
+#
+# logger.info("Success: connection to aurora established")
+#
+# # try:
+# #     with connection.cursor() as cursor:
+# #         sql = "CREATE TABLE users ( \
+# #                 id int(11) NOT NULL AUTO_INCREMENT, \
+# #                 email varchar(255) COLLATE utf8_bin NOT NULL, \
+# #                 password varchar(255) COLLATE utf8_bin NOT NULL, \
+# #                 PRIMARY KEY (id) \
+# #                 ) AUTO_INCREMENT=1"
+# #         cursor.execute(sql)
+#
+# try:
 #     with connection.cursor() as cursor:
-#         sql = "CREATE TABLE users ( \
-#                 id int(11) NOT NULL AUTO_INCREMENT, \
-#                 email varchar(255) COLLATE utf8_bin NOT NULL, \
-#                 password varchar(255) COLLATE utf8_bin NOT NULL, \
-#                 PRIMARY KEY (id) \
-#                 ) AUTO_INCREMENT=1"
-#         cursor.execute(sql)
-
-try:
-    with connection.cursor() as cursor:
-        sql = "INSERT INTO users ( email, password) values (%s, %s)"
-
-        cursor.execute(sql,('rganesan@beachbody.com','justsomething'))
-
-        connection.commit()
-
-finally:
-    connection.close()
-
-print(mconfig.tablette)
+#         sql = "INSERT INTO users ( email, password) values (%s, %s)"
+#
+#         cursor.execute(sql,('rganesan@beachbody.com','justsomething'))
+#
+#         connection.commit()
+#
+# finally:
+#     connection.close()
+#
+# print(mconfig.tablette)
