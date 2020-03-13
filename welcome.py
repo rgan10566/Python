57c44f98e93ce507342233119b92327f8749898b
Ramesh Ganesan <ramesh@Rameshs-Air.home>
2020-03-12 21:11:48 -0700

    commit

--- welcome.py
+++ welcome.py
@@ -1,2 +1,20 @@
+import platform
 print("Welcome to Python")
 print("Just making a Change to check commit")
+print(platform.platform())
+print(platform.platform(0))
+print(platform.platform(0))
+print(platform.platform(0,0))
+print(platform.platform(0,1))
+print(platform.platform(1,0))
+print(platform.platform(1,1))
+
+print(platform.machine())
+print(platform.processor())
+print(platform.system())
+print(platform.version())
+
+print(platform.python_implementation())
+
+for atr in platform.python_version_tuple():
+    print(atr)
